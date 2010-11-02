031497a7841312d2d94c5ec4b70a512175584065
athanassiadis <georgios.athanassiadis@gmail.com>
2010-11-02 16:15:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YmRailsTemplate::Application.config.secret_token = 'c83109eabbafa076428cd7659a0eb1ffc1f22d8f2720178ccd8b27e8c0f8f32d33bcc5087aa0c543eee0f8e81012dee65463d745caf6cb7c62fe950dcfb850bc'
